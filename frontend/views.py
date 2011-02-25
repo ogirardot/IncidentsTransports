02eabc685364f922df53c203000ab906d5d811f4
incidents-ratp <incidents-ratp@mybox.(none)>
2011-02-25 10:13:20 +0100

    commit spammer

--- frontend/views.py
+++ frontend/views.py
@@ -50,8 +50,13 @@ def add_incident(request):
 	if request.method == "POST":
 		form = AddIncidentForm(request.POST)
 		if form.is_valid():
-			form.save()
-			return render('thanks.html', {'number' : Incident.objects.count()})
+			contributed = request.session.get('contributed', None)
+			if contributed
+				return render('thanks.html', {'number' : Incident.objects.count()})
+			else:
+				form.save()
+				request.session['contributed'] = str(datetime.now())
+				return render('thanks.html', {'number' : Incident.objects.count()})
 		else:
 			return render('add_incident.html', {'form' : form})
 	else:
