51a4876a8d25c89f82e86e3e135af9804d84e6e5
incidents-transports <incidents-transports@mybox.(none)>
2011-03-01 20:26:43 +0100

    staged fabric file for deploy

--- fabfile.py
+++ fabfile.py
@@ -20,3 +20,13 @@ def deploy_preprod():
         # re-set setting properly
         local('cp -f settings.py.pre-prod settings.py', capture=False)
         # re-syncdb
+
+def deploy_prod():
+        # reset hard env
+        local('git reset --hard', capture=False)
+        # update with git
+        local('git pull origin master', capture=False)
+        # reset application
+        # re-set setting properly
+        local('cp -f settings.py.prod settings.py', capture=False)
+        # re-syncdb
