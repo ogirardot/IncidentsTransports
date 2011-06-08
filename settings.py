575dce7f919429ab942e4dd2f768a60e6febc1e9
Olivier Girardot <ssaboum@gmail.com>
2011-06-08 20:00:10 +0200

    useless settings are useless

--- settings.py
+++ settings.py
@@ -18,8 +18,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': DJANGO_ROOT_PATH + '/db/db.sqlite',                      # Or path to database file if using sqlite3.
-        'USER': 'incidents-ratp',                      # Not used with sqlite3.
-        'PASSWORD': 'ratp94550',                  # Not used with sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
