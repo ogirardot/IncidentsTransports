f019ebb9576095a3655271d53f78e447099099b0
Olivier Girardot <ssaboum@gmail.com>
2011-06-10 15:59:11 +0200

    reformated api to allow for sort_by GET param for sorting incidents read calls

--- api/urls.py
+++ api/urls.py
@@ -9,7 +9,6 @@ vote_handler = Resource(IncidentVoteHandler)
 
 urlpatterns = patterns('',
    	url(r'^incidents.(?P<emitter_format>[a-z]+)/(?P<scope>[a-z]+)$', incident_handler),  
-   	url(r'^incidents.(?P<emitter_format>[a-z]+)/(?P<scope>[a-z]+)/(?P<order_field>[a-z]+)$', incident_handler),
 	url(r'^incident.(?P<emitter_format>[a-z]+)/(?P<incident_id>[\d]+)', incident_handler),
 	url(r'^incident.(?P<emitter_format>[a-z]+)/vote/(?P<incident_id>[\d]+)/(?P<action>[a-z]+)', vote_handler),
 	url(r'^incident/?$', crud_incident_handler), 
